9d965186447a645adc41e0cb32826887089271e1
Robert Cox <rob@point.com>
2018-03-19 20:29:02 -0700

    Playing around

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    text = "¡Hola, mundo!"
+    render(inline: "<% (0..3).each do %><h1>#{text}</h1><% end %>")
+  end
+  def goodbye
+    text = "¡Adios, puto!"
+    render(inline: "<% (0..3).each do %><h1>#{text}</h1><% end %>")
+  end
 end
